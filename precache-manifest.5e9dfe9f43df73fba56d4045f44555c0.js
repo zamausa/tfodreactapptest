7c1614db4f4ef27ad56c82462304f634b1e54a41
zamausa <marco.ausani36@gmail.com>
2023-10-22 23:21:08 +0200

    Updates

---
+++ precache-manifest.5e9dfe9f43df73fba56d4045f44555c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f79d368ebf07fdca25c5ffee46fecfaa",
+    "url": "/zamausa/tfodreactapptestindex.html"
+  },
+  {
+    "revision": "96c7c8122cc0d0b3e35f",
+    "url": "/zamausa/tfodreactappteststatic/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "8e39faae2fcec9448f21",
+    "url": "/zamausa/tfodreactappteststatic/js/2.21ff9bcf.chunk.js"
+  },
+  {
+    "revision": "bc620bd9cc8893328a5f453e25589d03",
+    "url": "/zamausa/tfodreactappteststatic/js/2.21ff9bcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96c7c8122cc0d0b3e35f",
+    "url": "/zamausa/tfodreactappteststatic/js/main.28d9945a.chunk.js"
+  },
+  {
+    "revision": "56ec036fb69bd39e4ac8",
+    "url": "/zamausa/tfodreactappteststatic/js/runtime-main.cafad454.js"
+  }
+]);
